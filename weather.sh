23758b320ccd05a11d33a7c4aab9cd77058467df
Ben Kogan <benkogan@me.com>
2014-03-15 17:09:24 -0400

    Progress on weather.sh, but 2 issues with tmux mode

--- weather.sh
+++ weather.sh
@@ -10,36 +10,46 @@
 cachedir="$HOME/scripts/.cache"
 cache="$HOME/scripts/.cache/weather.log"
 time=$(date +%s)
-modold=210 # 3 min 30 seconds
+modold=210 # 03 minutes 30 seconds
+modmid=180 # 03 minutes
 modnew=10  # 10 seconds
-modlod=180 # 3 minutes
 
-# tmux: If .cache dir doesn't exist, make it
-if [[ ! -d "$cachedir" && "$1" == "-t" ]]; then
-    mkdir "$cachedir"
-    touch "$cache"
-    error=$?
-    if [ ! "$error" -eq 0 ]; then
-        echo "mkdir $cachedir failed"
-        exit $error
+# TODO: fix 2 issues:
+#       1. if clauses for mod- aren't working
+#       2. read isn't working
+
+if [[ "$1" == "-t" ]]; then
+
+    # tmux: If .cache dir doesn't exist, make it
+    if [[ ! -d "$cachedir" ]]; then
+        echo "TEST: cache dir created"
+        mkdir "$cachedir"
+        touch "$cache"
+        error=$?
+        if [ ! "$error" -eq 0 ]; then
+            echo "mkdir $cachedir failed"
+            exit $error
+        fi
     fi
-fi
 
-if [ "$1" == "-t" ]; then
     cachemod=$(stat -f%m "$cache") # Mod time of cache in sec since the epoch
-    let diff=($time-$cachemod)     # Seconds from now since file was modified
+    let diff=($time - $cachemod)   # Seconds from now since file was modified
+    echo "TEST: time is $time, cachemod is $cachemod, diff is $diff seconds"
 
-    # First case:  weather called by tmux on startup; assumes this is the case if
-    #              file hasn't been used for longer than modold
+    # Load cached version
+    if [[ diff < modmid ]]; then
+        echo "TEST: cache loaded, should exit"
+        read weather < $cache
+        echo "w: $weather"
+        exit $?
+
+    # First case:  on tmux startup; assumes this is the case if cache file
+    #              hasn't been used for longer than modold
     # Second case: cache is newly created; should thus be empty
-    if [[ diff > modold || diff < modnew ]]
-        # TODO: maybe this is a bad idea? delays tmux twice as long
+    elif [[ diff > modold || diff < modnew ]]; then
+        # TODO: maybe this is a bad idea? delays loading twice as long
         echo "loading"
-  # TODO: IF mod > 3:30 min || < 10 sec from current time
-        # display loading
-  # TODO: ELIF mod < 3 min
-        # load cached version
-        # exit
+   fi
 fi
 
 # Get zipcode
@@ -52,20 +62,21 @@ if [[ "$2" == "-c" || "$1" == "-c" ]]; then
         "http://xml.weather.yahoo.com/forecastrss?p=$zipcode&u=c" \
         | grep -E '(Current Conditions:|C<BR)' \
         | sed -e 's/Current Conditions://' -e 's/<br \/>//' -e 's/<b>//' \
-        -e 's/<\/b>//' -e 's/<BR \/>//' -e 's/<description>//' \
+        -e 's/<\/b>//' -e 's/C<BR \/>/ºC/' -e 's/<description>//' \
         -e 's/<\/description>//' | tr -d '\n')
 else
     weather=$(curl --silent \
         "http://xml.weather.yahoo.com/forecastrss?p=$zipcode" \
         | grep -E '(Current Conditions:|F<BR)' \
         | sed -e 's/Current Conditions://' -e 's/<br \/>//' -e 's/<b>//' \
-        -e 's/<\/b>//' -e 's/<BR \/>//' -e 's/<description>//' \
+        -e 's/<\/b>//' -e 's/F<BR \/>/°F/' -e 's/<description>//' \
         -e 's/<\/description>//' | tr -d '\n')
 fi
 echo "$weather"
 
 # tmux: add weather to cache file
 if [ "$1" == "-t" ]; then
+    echo "TEST: stores"
     echo "$weather" > "$cache"
 fi
 
